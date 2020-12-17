fbf8bae1b6558390f56d85d2c5064efdf2322f1b
quanghuy74 <huy25107@email.com>
2020-12-17 16:05:43 +0700

    chapte 6: modeling user

---
+++ config/initializers/config.rb
@@ -0,0 +1,6 @@
+Config.setup do |config|
+  config.const_name = "Settings"
+
+  config.env_prefix = "SETTINGS"
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  scope "(:locale)", locale: /en|vi/ do
-    root "static_pages#home"
+  root "static_pages#home"
+  
+  get "/help", to: "static_pages#help"
+  get "/about", to: "static_pages#about"
+  get "/contact", to: "static_pages#contact"
 
-    get "/help", to: "static_pages#help"
-    get "/about", to: "static_pages#about"
-    get "/contact", to: "static_pages#contact"
-  end
+  get "/signup", to: "users#new"
+  resources :users, only: %i(create show)
 end
