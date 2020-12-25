9cca28949bd30aea3a28eeeb0540693d3ec69467
quanghuy74 <huy25107@email.com>
2020-12-25 10:39:01 +0700

    chapter 8: log in

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,13 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :password, length: {minimum: Settings.user.password.min_length}
+
+  def self.digest string
+    cost =  if ActiveModel::SecurePassword.min_cost
+              BCrypt::Engine::MIN_COST
+            else
+              BCrypt::Engine.cost
+            end
+    BCrypt::Password.create string, cost: cost
+  end
 end
