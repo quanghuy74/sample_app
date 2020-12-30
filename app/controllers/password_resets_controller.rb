f57f0bcd0e7c014c77abc0537a626fe7b5c8097f
quanghuy74 <huy25107@email.com>
2020-12-30 12:59:55 +0700

    chapter_12: reset password

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,53 @@
+class PasswordResetsController < ApplicationController
+  before_action :get_user, :valid_user, only: %i(edit update)
+
+  def new; end
+
+  def edit; end
+
+  def create
+    @user = User.find_by email: params[:password_reset][:email].downcase
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = t "user_reset_password.email_sent"
+      redirect_to root_url
+    else
+      flash.now[:danger] = t "user_reset_password.email_not_found"
+      render :new
+    end
+  end
+
+  def update
+    if user_params[:password].empty?
+      @user.errors.add(:password, t("user_reset_password.pw_empty"))
+      render :edit
+    elsif @user.update user_params
+      log_in @user
+      flash[:success] = t "user_reset_password.success"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def get_user
+    return if @user = User.find_by email: params[:email]
+
+    flash[:danger] = t "user_reset_password.email_not_found"
+    redirect_to new_password_reset_path
+  end
+
+  def valid_user
+    return if @user.authenticated?(:reset, params[:id])
+
+    flash[:danger] = t "user_reset_password.incorrect_user"
+    redirect_to new_password_reset_path
+  end
+
+  def user_params
+    params.require(:user).permit :password, :password_confirmation
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ User.create!(name: "Example User",
              email: "huy25107@gmail.com",
              admin: true,
              password: "123123",
-             password_confirmation: "123123"
+             password_confirmation: "123123",
              activated: true, activated_at: Time.zone.now)
 # Generate a bunch of additional users.
 30.times do |n|
@@ -12,6 +12,6 @@ User.create!(name: "Example User",
   User.create!(name: name,
                email: email,
                password: password,
-               password_confirmation: password
+               password_confirmation: password,
                activated: true, activated_at: Time.zone.now)
 end
